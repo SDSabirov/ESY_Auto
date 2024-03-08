d0fb1d02f55ba67fdfe102e4a4a700d59cc3cfa2
Saidmurod Sabirov <sabir.dev7@gmail.com>
2024-03-08 21:31:31 +0300

    front-end dep set-up

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path';
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  alias: {
+    '@': resolve(__dirname, '/'),
+  },
+  css: ['~/assets/css/main.css'],
+  modules: [
+    'nuxt-aos',
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
